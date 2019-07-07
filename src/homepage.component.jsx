ce847b079aba26bbf8664c6ef2c4686f8aa42693
vdugeri <verem.dugeri@gmail.com>
2019-07-07 18:59:44 +0100

    Project setup

---
+++ src/homepage.component.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="menu-directory">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">HATS</h2>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="menu-directory">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">APPARELS</h2>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="menu-directory">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">SNEAKERS</h2>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="menu-directory">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">MEN'S</h2>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="menu-directory">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">WOMEN'S</h2>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage;

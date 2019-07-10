67009aa5ebdd0db127d85c4f9b2bf9f317705d19
vdugeri <verem.dugeri@gmail.com>
2019-07-10 21:29:19 +0100

    Create collection and collection-item components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
-import HatsPage from './pages/hats/hatspage.component';
-import { Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop/hats" component={HatsPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }

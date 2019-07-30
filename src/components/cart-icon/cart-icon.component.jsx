1bc31485d34fdbd17ca7f12978ff79ca80e8f932
vdugeri <verem.dugeri@gmail.com>
2019-07-30 10:53:36 +0100

    Create cart item component and connect cart dropdown with cart items reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,18 +7,22 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, cartItems }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{cartItems.length}</span>
   </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);

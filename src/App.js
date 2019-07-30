b3e8cc3e8b22a0215c1cf2065d950b7844b2c593
vdugeri <verem.dugeri@gmail.com>
2019-07-30 13:36:58 +0100

    implement checkout page and checkout item component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import AuthPage from "./pages/auth/auth.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { auth, createUserProfileDocument } from "./firebase/fireabase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
@@ -45,6 +46,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             path="/signin"
             render={() =>
